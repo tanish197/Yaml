4d798364c7d32c8deba005de123b86c2adb20d08
Tanish <tanishj1907@gmail.com>
2023-07-30 16:40:59 +0530

    yaml_practice

---
+++ test_yml.py
@@ -0,0 +1,9 @@
+import yaml
+
+with open('first.yml', 'r') as file:
+    try:
+        print(yaml.safe_load(file))
+    except yaml.YAMLError as exec:
+        print(exec)
+              
+          
